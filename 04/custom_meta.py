3b466ad3b4f23ae9f934753eca2e8f1801d21563
Nazar <furnoxl@yandex.ru>
2023-04-05 00:17:21 +0700

    Sort results

--- 04/custom_meta.py
+++ 04/custom_meta.py
@@ -8,18 +8,22 @@ class CustomMeta(type):
         for key in list_changes_keys:
             classdict[f"custom_{key}"] = classdict.pop(key)
         cls = super().__new__(mcs, name, bases, classdict)
-        print('Meta __new__', cls, bases, classdict, kwargs)
         return cls
 
-    def __init__(cls, name, bases, classdict, **kwargs):
+    def __call__(cls, *args, **kwargs):
+        obj = cls.__new__(cls, *args, **kwargs)
+        obj.__init__(*args, **kwargs)
         list_changes_keys = [
             key
-            for key in classdict.keys() if not (key.startswith("__") and key.endswith("__"))
+            for key in obj.__dict__.keys() if not (key.startswith("__") and key.endswith("__"))
         ]
         for key in list_changes_keys:
-            classdict[f"custom_{key}"] = classdict.pop(key)
-        print(classdict)
-        super().__init__(name, bases, classdict)
+            obj.__setattr__(f"custom_{key}", obj.__dict__[key])
+            obj.__delattr__(key)
+        return obj
+
+    def __setattr__(self, key, value):
+        print(self, key, value)
 
 
 class CustomClass(metaclass=CustomMeta):
@@ -42,10 +46,10 @@ assert inst.custom_val == 99
 assert inst.custom_line() == 100
 assert str(inst) == "Custom_by_metaclass"
 
-inst.x  # ошибка
-inst.val  # ошибка
-inst.line()  # ошибка
-inst.yyy  # ошибка
+# inst.x  # ошибка
+# inst.val  # ошибка
+# inst.line()  # ошибка
+# inst.yyy  # ошибка
 
 inst.dynamic = "added later"
 assert inst.custom_dynamic == "added later"
