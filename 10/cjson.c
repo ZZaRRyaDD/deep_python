178f8ae6a8a6036fc0b48890ad2a9ef865de29e0
Назар <furnoxl@yandex.ru>
2023-05-22 02:16:30 +0700

    Try add hw 10

--- 10/cjson.c
+++ 10/cjson.c
@@ -5,11 +5,12 @@
 
 int compare_symbols(PyObject* firstChar, const char secondChar)
 {
-    return PyObject_RichCompareBool(
-        firstChar,
-        PyUnicode_FromString(&secondChar),
-        Py_EQ
-    );
+    return PyUnicode_Compare(firstChar, PyUnicode_FromString(&secondChar)) == 0;
+}
+
+int compare_symbols_ptr(PyObject* firstChar, const char* secondChar)
+{
+    return PyUnicode_Compare(firstChar, PyUnicode_FromString(secondChar)) == 0;
 }
 
 PyObject* cjson_loads(PyObject* self, PyObject* args)
@@ -35,11 +36,18 @@ PyObject* cjson_loads(PyObject* self, PyObject* args)
     const char* emptyString = "";
     const char* openBracket = "{";
     const char* closeBracket = "}";
-    const char doubleQuote = '"';
-    const char colon = ':', emptyChar = ' ', commaChar = ',';
-    const char oneChar = '1', twoChar = '2', threeChar = '3', fourChar = '4';
-    const char fiveChar = '5', sixChar = '6', sevenChar = '7', eightChar = '8';
-    const char nineChar = '9';
+    const char* commaChar = ",";
+    const char* doubleQuote = "\"";
+    const char* colon = ":";
+    const char* oneChar = "1";
+    const char* twoChar = "2";
+    const char* threeChar = "3";
+    const char* fourChar = "4";
+    const char* fiveChar = "5";
+    const char* sixChar = "6";
+    const char* sevenChar = "7";
+    const char*eightChar = "8";
+    const char* nineChar = "9";
     if (length_unicode_json_str)
     {
         if(
@@ -56,12 +64,21 @@ PyObject* cjson_loads(PyObject* self, PyObject* args)
         PyErr_Format(PyExc_TypeError, "String is empty");
         return NULL;
     }
+
     for (Py_ssize_t i = 0; i < length_unicode_json_str; i++)
     {
         PyObject* element = PyUnicode_Substring(json_str, i, i + 1);
         printf("symbol - %s\n", PyUnicode_AsUTF8(element));
+        if (key != NULL)
+        {
+            printf("key - %s %d\n", PyUnicode_AsUTF8(key), PyObject_Length(key));
+        }
+        if (value != NULL)
+        {
+            printf("value - %s %d\n", PyUnicode_AsUTF8(value), PyObject_Length(value));
+        }
 
-        if (key == NULL && value == NULL && compare_symbols(element, doubleQuote))
+        if (key == NULL && value == NULL && compare_symbols_ptr(element, doubleQuote))
         {
             lenKey++;
             key = PyList_New(0);
@@ -69,6 +86,7 @@ PyObject* cjson_loads(PyObject* self, PyObject* args)
                 printf("ERROR: Failed to create List Object\n");
                 return NULL;
             }
+            printf("создаем ключ\n");
             continue;
         }
 
@@ -76,7 +94,7 @@ PyObject* cjson_loads(PyObject* self, PyObject* args)
         {
             if (keyCreated == true)
             {
-                if (compare_symbols(element, doubleQuote))
+                if (compare_symbols_ptr(element, doubleQuote))
                 {
                     value = PyList_New(0);
                     if (value == NULL)
@@ -85,19 +103,18 @@ PyObject* cjson_loads(PyObject* self, PyObject* args)
                         return NULL;
                     }
                     isString = true;
-                    continue;
                 }
                 else if
                 (
-                    compare_symbols(element, oneChar) ||
-                    compare_symbols(element, twoChar) ||
-                    compare_symbols(element, threeChar) ||
-                    compare_symbols(element, fourChar) ||
-                    compare_symbols(element, fiveChar) ||
-                    compare_symbols(element, sixChar) ||
-                    compare_symbols(element, sevenChar) ||
-                    compare_symbols(element, eightChar) ||
-                    compare_symbols(element, nineChar)
+                    compare_symbols_ptr(element, oneChar) ||
+                    compare_symbols_ptr(element, twoChar) ||
+                    compare_symbols_ptr(element, threeChar) ||
+                    compare_symbols_ptr(element, fourChar) ||
+                    compare_symbols_ptr(element, fiveChar) ||
+                    compare_symbols_ptr(element, sixChar) ||
+                    compare_symbols_ptr(element, sevenChar) ||
+                    compare_symbols_ptr(element, eightChar) ||
+                    compare_symbols_ptr(element, nineChar)
                 )
                 {
                     value = PyList_New(0);
@@ -107,27 +124,28 @@ PyObject* cjson_loads(PyObject* self, PyObject* args)
                         return NULL;
                     }
                     isNumber = true;
-                    continue;
+                    lenValue++;
+                    if (PyList_Append(value, element) == -1)
+                    {
+                        printf("ERROR: Failed to add item to List Object\n");
+                        return NULL;
+                    }
                 }
-                else if
-                (
-                    compare_symbols(element, colon) ||
-                    compare_symbols(element, emptyChar)
-                ) 
-                {
-                    continue;
-                }
+                continue;
             }
 
-            if (lenKey > 0 && compare_symbols(element, doubleQuote))
+            if (lenKey > 0 && compare_symbols_ptr(element, doubleQuote))
             {   
-                const char* string = PyUnicode_AsUTF8(
-                    PyUnicode_Join(PyUnicode_FromString(emptyString), key)
-                );
-                if (!(key = Py_BuildValue("s", string))) {
+                const char* string = PyUnicode_AsUTF8(PyUnicode_Join(
+                    PyUnicode_FromString(emptyString),
+                    key
+                ));
+                key = Py_BuildValue("s", string);
+                if (key == NULL) {
                     printf("ERROR: Failed to build string value\n");
                     return NULL;
                 }
+                printf("ключ собрали %s\n", string);
                 lenKey = 0;
                 keyCreated = true;
             }
@@ -144,10 +162,10 @@ PyObject* cjson_loads(PyObject* self, PyObject* args)
 
         if (key != NULL && value != NULL)
         {
-            if (
-                compare_symbols(element, doubleQuote) &&
-                isString
-            )
+            printf("тут");
+            printf("dq %d", compare_symbols_ptr(element, doubleQuote));
+            printf("comma %d", compare_symbols_ptr(element, commaChar));
+            if (compare_symbols_ptr(element, doubleQuote) && isString)
             {
                 const char* string = PyUnicode_AsUTF8(
                     PyUnicode_Join(PyUnicode_FromString(emptyString), value)
@@ -156,18 +174,50 @@ PyObject* cjson_loads(PyObject* self, PyObject* args)
                     printf("ERROR: Failed to build string value\n");
                     return NULL;
                 }
+                lenValue = 0;
+                if (PyDict_SetItem(dict, key, value) < 0) {
+                    printf("ERROR: Failed to set item\n");
+                    return NULL;
+                }
+                isNumber = false;
+                isString = false;
+                key = (PyObject*)NULL;
+                value = (PyObject*)NULL;
+                printf("собрали значение");
+                if (key == NULL) {
+                    printf("key none");
+                }
+                if (value == NULL) {
+                    printf("value none");
+                }
             }
             else if (
-                compare_symbols(element, commaChar) &&
+                compare_symbols_ptr(element, commaChar) &&
                 isNumber
                 )
             {
-                const char* string = PyUnicode_AsUTF8(
-                    PyUnicode_Join(PyUnicode_FromString(emptyString), value)
-                );
-                if (!(value = Py_BuildValue("i", string))) {
+                PyObject* string = PyUnicode_Join(PyUnicode_FromString(emptyString), value);
+                PyObject* tempLong = PyLong_FromUnicodeObject(string, 10);
+                value = Py_BuildValue("l", PyLong_AsLong(tempLong));
+                if (value == NULL) {
                     printf("ERROR: Failed to build integer value\n");
                     return NULL;
+                }
+                lenValue = 0;
+                if (PyDict_SetItem(dict, key, value) < 0) {
+                    printf("ERROR: Failed to set item\n");
+                    return NULL;
+                }
+                isNumber = false;
+                isString = false;
+                key = (PyObject*)NULL;
+                value = (PyObject*)NULL;
+                printf("собрали значение");
+                if (key == NULL) {
+                    printf("key none");
+                }
+                if (value == NULL) {
+                    printf("value none");
                 }
             }
             else
@@ -178,39 +228,34 @@ PyObject* cjson_loads(PyObject* self, PyObject* args)
                     printf("ERROR: Failed to add item to List Object\n");
                     return NULL;
                 }
+                continue;
             }
-            lenValue = 0;
-            if (PyDict_SetItem(dict, key, value) < 0) {
-                printf("ERROR: Failed to set item\n");
-                return NULL;
-            }
-            PyMem_Free(key);
-            PyMem_Free(value);
         }
     }
     return dict;
 }
 
-// PyObject* cjson_dumps(PyObject* self, PyObject* args)
-// {
-//     PyObject* dict;
-//     if (!PyArg_ParseTuple(args, "O", &dict))
-//     {
-//        PyErr_Format(PyExc_TypeError, "Expected dict object");
-//        return NULL;
-//     }
-//     PyObject* listItems = PyDict_Items(&dict);
-//     long listSize = PyList_Size(listItems);
-//     for (long i = 0; i < listSize; i++) 
-//     {
-//         PyObject* element = PyList_GetItem(listItems, i);
-//         element
-//     }
-// }
+PyObject* cjson_dumps(PyObject* self, PyObject* args)
+{
+    PyObject* dict;
+    PyObject* json_str;
+    if (!PyArg_ParseTuple(args, "O", &dict))
+    {
+       PyErr_Format(PyExc_TypeError, "Expected dict object");
+       return NULL;
+    }
+    PyObject* listItems = PyDict_Items(&dict);
+    long listSize = PyList_Size(listItems);
+    for (long i = 0; i < listSize; i++) 
+    {
+        PyObject* element = PyList_GetItem(listItems, i);
+    }
+    return json_str;
+}
 
 static PyMethodDef methods[] = {
     {"loads", cjson_loads, METH_VARARGS, "Loads dict from string"},
-    // {"dumps", cjson_dumps, METH_VARARGS, "Convert dict to string"},
+    {"dumps", cjson_dumps, METH_VARARGS, "Convert dict to string"},
     {NULL, NULL, 0, NULL}
 };
 
